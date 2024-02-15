dde9cb6cba39d89b6efba56c8fd025a1362b239b
lkarim-l <lahsenkrm@gmail.com>
2024-02-15 12:27:15 +0100

    leo exemple

--- C01/ex00/ft_ft.c
+++ C01/ex00/ft_ft.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 
+	// [chantdoiseau]
 void ft_ft(int *nbr)
 {
+	//[chantdoiseau] = 42
 	*nbr = 42;
-	
 }
 
 int main()
 {
+
+	// [chantdoiseau] = 65 == x
 	int x = 65;
+	// x
 	printf("%d\n", x);
+	// [chantdoiseau]
 	ft_ft(&x);
+	// x
 	printf("%d\n", x);
+
+
+	int y;
+	
+	y = 17890;
+	printf("%d\n", y);
+	
+	ft_ft(&y);
+	printf("%d\n", y);
+
 	return 0;
 }
